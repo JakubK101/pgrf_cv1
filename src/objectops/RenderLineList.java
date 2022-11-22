d3d86e1a82c10ab58d6a322b16a92611de605aa6
JakubK101 <jakub.kesl1@seznam.cz>
2022-11-22 13:08:43 +0100

    22.11

--- src/objectops/RenderLineList.java
+++ src/objectops/RenderLineList.java
@@ -3,11 +3,13 @@ package objectops;
 import objectdata.Scene;
 import objectdata.Solid;
 import rasterdata.RasterImage;
-import rasterdata.RasterImageBI;
 import rasterops.Liner;
-import rasterops.TrivialLiner;
 import transforms.Camera;
 import transforms.Mat4;
+import transforms.Point3D;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class RenderLineList<P> implements Renderer<P>{
     private Liner<P> liner;
@@ -16,14 +18,16 @@ public class RenderLineList<P> implements Renderer<P>{
 
     private Camera camera;
     private Mat4 modelMat;
+    private Mat4 projectionMatrix;
 
 
     public RenderLineList(P pixelVaule, Liner<P> liner, RasterImage<P> img, Camera camera, Mat4 modelMat){
-        this.liner= new TrivialLiner<>();
+        this.liner= liner;
         this.pixelValue=pixelVaule;
         this.img=img;
         this.camera= camera;
         this.modelMat=modelMat;
+
 
 
     }
@@ -40,10 +44,24 @@ public class RenderLineList<P> implements Renderer<P>{
     @Override
     public void renderSolid(Solid solid, Mat4 modelMat) {
         //compute final transformation matrix
+        final Mat4 transMatrix = modelMat.mul(camera.getViewMatrix().mul(projectionMatrix));
         //transformation of vertices
+        final List<Point3D> vertices = solid.vertices().stream()
+                .map(p -> p.mul(transMatrix))
+                .toList();
         //cut according to w
+        IntStream.iterate(0,i->i+2).limit(solid.vertices().size()/2).parallel()
+                .forEach(i ->{
+                    final Point3D start = solid.vertices().get(solid.indices().get(i));
+                    final Point3D end = solid.vertices().get(solid.indices().get(i+1));
+                }
+                //for x and y lies within
+
+        );
         //dehomog()
+
         //transformation to viewport
+
         //draw lines
     }
 }
--- src/rasterops/SeedFill4.java
+++ src/rasterops/SeedFill4.java
@@ -3,6 +3,7 @@ package rasterops;
 import rasterdata.RasterImage;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class SeedFill4<P> implements SeedFill<P> {
@@ -18,6 +19,20 @@ public class SeedFill4<P> implements SeedFill<P> {
         fill(img,c,r+1,pixelValue,isInArea);
         fill(img,c-1,r,pixelValue,isInArea);
         fill(img,c+1,r-1,pixelValue,isInArea);
+
+        /*img.getPixel(c,r).ifPresent(new Consumer<P>() {
+            @Override
+            public void accept(P p) {
+                if(isInArea.test(p)){
+                    img.setPixel(c,r,pixelValue);
+                    fill(img,c+1,r,pixelValue,isInArea);
+                    fill(img,c,r+1,pixelValue,isInArea);
+                    fill(img,c-1,r,pixelValue,isInArea);
+                    fill(img,c+1,r-1,pixelValue,isInArea);
+                }
+            }
+        });*/
+
 
     }
 }
